5adc0c14333a1d6ba19ec7ae3bcfa5312b982909
javierporta <javierporta@hotmail.com>
2019-11-08 15:19:52 +0000

    Added navigation

---
+++ ContactMe/ForgotYourPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ForgotYourPasswordViewController.swift
+//  ContactMe
+//
+//  Created by Javier Portaluppi on 08/11/2019.
+//  Copyright © 2019 IPL-Master. All rights reserved.
+//
+
+import UIKit
+
+class ForgotYourPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
