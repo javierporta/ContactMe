dacf43cbd53dc97a7a7a1a92f7563cc402a2be02
javierporta <javierporta@hotmail.com>
2019-11-21 16:16:11 +0000

    Ordered folders

---
+++ ContactMe/View Controllers/MyProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyProfileViewController.swift
+//  ContactMe
+//
+//  Created by Javier Portaluppi on 21/11/2019.
+//  Copyright © 2019 IPL-Master. All rights reserved.
+//
+
+import UIKit
+
+class MyProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
