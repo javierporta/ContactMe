12ea511212a42f674c232a72e63fb2923980212a
javierporta <javierporta@hotmail.com>
2019-11-24 01:25:28 +0000

    Added scroll view to my profile view

---
+++ ContactMe/Custom Controls/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  ContactMe
+//
+//  Created by Javier Portaluppi on 24/11/2019.
+//  Copyright © 2019 IPL-Master. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    func setIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame:
+            CGRect(x: 10, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame:
+            CGRect(x: 20, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+}
--- ContactMe/View Controllers/MyProfileViewController.swift
+++ ContactMe/View Controllers/MyProfileViewController.swift
@@ -9,22 +9,64 @@
 import UIKit
 
 class MyProfileViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var careerLabel: UILabel!
+    
+    @IBOutlet weak var universityTextField: UITextField! {
+        didSet {
+            universityTextField.tintColor = UIColor.lightGray
+            universityTextField.setIcon(#imageLiteral(resourceName: "icon-uni"))
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    //    MARK: Actions
+    @IBAction func changedSegmentedControl(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            scrollToPosition(0)
+        case 1: //Career
+            // Get career label position
+            let positionToScroll = careerLabel.frame.origin.y
+            scrollToPosition(positionToScroll)
+        case 2:
+            scrollToPosition(50)
+        case 3:
+            scrollToPosition(100)
+        default:
+            break
+        }
     }
-    */
-
+    
+    private func scrollToPosition(_ positionY: CGFloat){
+        scrollView.setContentOffset(CGPoint(x: 0, y: positionY), animated: true)
+        
+        
+    }
+    
+    //TODO: Create a function to change active segmented controll when scrolling
+    
+    
+    
+    
 }
