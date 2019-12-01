dab16808096d898641ab01dd5391e027bd22475d
3lchamac0 <3lchamac0@gmail.com>
2019-12-01 21:29:08 +0000

    - validando formulario de registro

--- ContactMe/View Controllers/RegisterViewController.swift
+++ ContactMe/View Controllers/RegisterViewController.swift
@@ -12,40 +12,60 @@ import EGFormValidator
 class RegisterViewController: ValidatorViewController {
 
 //    MARK: Outlets
+
+
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var emailErrorLabel: UILabel!
-    @IBOutlet weak var passwordTextFIeld: UITextField!
-    @IBOutlet weak var repeatPasswordTextField: UITextField!
+  
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordErrorLabel: UILabel!
     
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordErrorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        /*self.addValidatorEmail(toControl: self.emailTextField,
+        self.addValidatorEmail(toControl: self.emailTextField,
         errorPlaceholder: self.emailErrorLabel,
-            errorMessage: "Email is invalid")*/
+            errorMessage: "Email is invalid")
+        self.addValidatorMandatory(toControl: self.emailTextField,
+        errorPlaceholder: self.emailErrorLabel,
+            errorMessage: "This field is required")
         
-        self.addValidatorMinLength(toControl: self.emailTextField,
-        errorPlaceholder: self.emailErrorLabel,
-            errorMessage: "Enter at least %d characters",
-               minLength: 8)
+        
+        self.addValidatorMinLength(toControl: self.passwordTextField,                 errorPlaceholder: self.passwordErrorLabel,
+            errorMessage: "Enter at least %d characters", minLength: 8)
+        self.addValidatorMandatory(toControl: self.passwordTextField,                   errorPlaceholder: self.passwordErrorLabel,
+            errorMessage: "This field is required")
+        
+        self.addValidatorMinLength(toControl: self.repeatPasswordTextField,                 errorPlaceholder: self.repeatPasswordErrorLabel,
+            errorMessage: "Enter at least %d characters", minLength: 8)
+        self.addValidatorMandatory(toControl: self.repeatPasswordTextField,                   errorPlaceholder: self.repeatPasswordErrorLabel,
+            errorMessage: "This field is required")
     }
     
 //    MARK:Actions
     @IBAction func signUpButton(_ sender: Any) {
         print("Sign Up pressed")
-    }
         
-    @IBAction func emailEditingChanged(_ sender: Any) {
         if self.validate(){
             print("esta bien")
         }else{
              print("esta mal")
         }
     }
-    
-
+            
+    @IBAction func emailEditingChanged(_ sender: Any) {
+        _ = self.validate()
+    }
+    @IBAction func passwordEditingChanged(_ sender: Any) {
+        _ = self.validate()
+    }
+    @IBAction func repeatPasswordEditingChanged(_ sender: Any) {
+        _ = self.validate()
+    }
     /*
     // MARK: - Navigation
 
