39e534c972c217535c77e3b4179a482952c4b42d
javierporta <javierporta@hotmail.com>
2019-12-11 19:11:23 +0000

    Resizing image before sending it

--- ContactMe/View Controllers/QRShowViewController.swift
+++ ContactMe/View Controllers/QRShowViewController.swift
@@ -99,6 +99,9 @@ class QRShowViewController: UIViewController {
         // Encode
         let jsonEncoder = JSONEncoder()
         do {
+            let tempImage = resizeImage(image: (currentUserProfile.avatar?.toImage())!, targetSize: CGSize(width: 10.0, height: 10.0))
+            
+            currentUserProfile.avatar=tempImage.toString()
             let jsonData = try jsonEncoder.encode(currentUserProfile)
             let json = String(data: jsonData, encoding: String.Encoding.utf8)
             
@@ -134,6 +137,32 @@ class QRShowViewController: UIViewController {
         
         
         
+    }
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
     }
     
     func showAllItems(){
