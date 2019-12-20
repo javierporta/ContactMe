b3c815947fb5e3d89e81ac34006fd232196e76f0
3lchamac0 <3lchamac0@gmail.com>
2019-12-20 17:37:02 +0000

    - data para img

---
+++ ContactMe/Models/DataImage.swift
@@ -0,0 +1,9 @@
+//
+//  DataImage.swift
+//  ContactMe
+//
+//  Created by royli on 18/12/2019.
+//  Copyright © 2019 IPL-Master. All rights reserved.
+//
+
+import Foundation
---
+++ ContactMe/Models/ResponseImage.swift
@@ -0,0 +1,9 @@
+//
+//  ResponseImage.swift
+//  ContactMe
+//
+//  Created by royli on 18/12/2019.
+//  Copyright © 2019 IPL-Master. All rights reserved.
+//
+
+import Foundation
