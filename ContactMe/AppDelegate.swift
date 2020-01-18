1d4348090197f81293cffca66bcbca3eeceaacee
javierporta <javierporta@hotmail.com>
2020-01-18 19:36:09 +0000

    Added back label to QR scanner view

--- ContactMe/AppDelegate.swift
+++ ContactMe/AppDelegate.swift
@@ -49,7 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
              try keychain.removeAll()
              
              deleteDatabase()
-             }*/
+             */
             
             
             
